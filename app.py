3fc0738168c270857fe2b23851805589acf01946
Lucas gonzalez-Rey <lucasglezrey120@gmail.com>
2024-01-31 19:34:32 -0500

    Update new route for hmtl page

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World! 👋 from Lucas Gonzalez-Rey. Adding first code change'
 
+
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
+
 
 if __name__ == '__main__':
     app.run()
